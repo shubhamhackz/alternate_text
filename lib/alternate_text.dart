8695b60a373f9c68395dbdfc68c42719fd0b4369
Shubham Soni <shubham@threedots.app>
2022-05-14 09:30:52 +0530

    added example

---
+++ lib/alternate_text.dart
@@ -0,0 +1,7 @@
+/// Flutter widget that automatically resizes text to fit perfectly within its
+/// bounds.
+library alternate_text;
+
+import 'package:flutter/material.dart';
+
+part 'src/alternate_text.dart';
